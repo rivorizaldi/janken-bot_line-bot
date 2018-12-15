3d1e95e3ec24a1355c29418632c50673907b4825
rivorizaldi <rivo.rizaldy18@gmail.com>
2018-12-15 16:28:43 +0700

    add line code

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -125,8 +125,10 @@ class Webhook extends CI_Controller {
 
   private function textMessage($event){
     $userMessage = $event['message']['text'];
-
-      if(strtolower($userMessage) == '1. mulai bermain')
+    if($this->user['number'] == 0)
+    {
+      
+      if(strtolower($userMessage) == '1. Mulai Bermain')
       {
         // reset score
         $this->tebakkode_m->setScore($this->user['user_id'], 0);
@@ -136,7 +138,7 @@ class Webhook extends CI_Controller {
         $this->sendQuestion($event['replyToken'], 1);
       } 
       
-      elseif(strtolower($userMessage) == '2. panduan') {
+      elseif(strtolower($userMessage) == '2. Panduan') {
         $code = array("wink" => "\u{10008F}", 
                     "Gunting" => "\u{100030}",
                     "Batu" => "\u{100031}",
@@ -203,6 +205,10 @@ class Webhook extends CI_Controller {
       $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
       }
       
+    // if user already begin test
+    } else {
+      $this->checkAnswer($userMessage, $event['replyToken']);
+    }
   }
 
   private function stickerMessage($event){
