da53a85deea7173a6c5b11c7557db11a57c39fe9
rivorizaldi <rivo.rizaldy18@gmail.com>
2018-12-17 22:19:23 +0700

    first commit

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -126,7 +126,7 @@ class Webhook extends CI_Controller {
 
   private function textMessage($event){
     $userMessage = $event['message']['text'];
-    if($userMessage !== 'Gunting' || $userMessage !== 'Kertas' || $userMessage !== 'Batu' || $userMessage !== 'Lihat Score')
+    if($userMessage !== 'Gunting' && $userMessage !== 'Kertas' && $userMessage !== 'Batu' && $userMessage !== 'Lihat Score')
       {
 
       if(strtolower($userMessage) == '1. mulai bermain')
