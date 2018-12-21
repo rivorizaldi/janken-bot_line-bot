38f50a5c5227405acca68980804cb58fe86d9a1e
rivorizaldi <rivo.rizaldy18@gmail.com>
2018-12-22 06:03:29 +0700

    first commit

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -498,7 +498,7 @@ class Webhook extends CI_Controller {
         break;
     }
   } 
-    else {
+    elseif ($playerChoice == "Lihat Score") {
       // create user score message
       $message = 'Skormu '. $this->tebakkode_m->get_comScore() . ' Skor Komp' . $this->user['score'];
       $textMessageBuilder1 = new TextMessageBuilder($message);
@@ -520,6 +520,32 @@ class Webhook extends CI_Controller {
       // merge all message
       $multiMessageBuilder = new MultiMessageBuilder();
       $multiMessageBuilder->add($textMessageBuilder1);
+      $multiMessageBuilder->add($messageBuilder);
+
+      // send reply message
+      $this->bot->replyMessage($replyToken, $multiMessageBuilder);
+    } else {
+
+      $message = "Keyword yang dimasukkan tidak sesuai dengan pilihan yang ada";
+      $textMessageBuilder = new TextMessageBuilder($message);
+
+      $opsi = ["Gunting","Kertas","Batu","Lihat Score"];
+      $opsiText = ["Kamu Mengeluarkan Gunting","Kamu Mengeluarkan Kertas","Kamu Mengeluarkan Batu","Lihat Score"];
+      $length = count($opsi);
+
+      for($i = 0; $i<$length; $i++){
+        $options[] = new MessageTemplateActionBuilder($opsi[$i],$opsiText[$i]);
+      } 
+
+      // prepare button template
+      $buttonTemplate = new ButtonTemplateBuilder(null, 'Janken-Bot Game!', null, $options);
+    
+      // build message
+      $messageBuilder = new TemplateMessageBuilder("Gunakan mobile app untuk melihat soal", $buttonTemplate);
+
+      // merge all message
+      $multiMessageBuilder = new MultiMessageBuilder();
+      $multiMessageBuilder->add($textMessageBuilder);
       $multiMessageBuilder->add($messageBuilder);
 
       // send reply message
